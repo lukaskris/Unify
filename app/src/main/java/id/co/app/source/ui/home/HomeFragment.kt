30eb6cf55f1dc7187fb43975a184b1e8d8b41e2e
hendrysyamsudin <hendry.syamsudin@gmail.com>
2020-11-18 17:24:28 +0700

    Add new home page

--- app/src/main/java/id/co/app/source/ui/home/HomeFragment.kt
+++ app/src/main/java/id/co/app/source/ui/home/HomeFragment.kt
@@ -1,27 +1,36 @@
 package id.co.app.source.ui.home
 
+import android.graphics.Color
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
-import androidx.navigation.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.appbar.AppBarLayout.OnOffsetChangedListener
 import id.co.app.source.R
 import id.co.app.source.databinding.FragmentHomeBinding
 import id.co.app.source.utilities.Common
+import kotlin.math.abs
+
 
 class HomeFragment : Fragment() {
 
-    lateinit var binding: FragmentHomeBinding
+    private lateinit var binding: FragmentHomeBinding
+    private var isToolbarShown = false
+    private val isToolbarShownKey = "isToolbarShown"
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        if (savedInstanceState != null) {
+            isToolbarShown = savedInstanceState.getBoolean(isToolbarShownKey)
+        }
         //binding = FragmentHomeBindingImpl.inflate(inflater)
         binding = DataBindingUtil.inflate<FragmentHomeBinding>(
             inflater,
@@ -31,27 +40,49 @@ class HomeFragment : Fragment() {
         ).apply {
             //viewModel = plantDetailViewModel
             lifecycleOwner = viewLifecycleOwner
-            var isToolbarShown = false
-            toolbarLayout.isTitleEnabled = true
+            appbar.addOnOffsetChangedListener(OnOffsetChangedListener { appBarLayout, verticalOffset ->
+                val totalScrollRange = appBarLayout.totalScrollRange
+                val midTotalScrollRange = totalScrollRange / 2
+                val totalVerticalOffset = verticalOffset * -1
+                //Log.v("verticalOffset", "verticalOffset : $totalVerticalOffset , $totalScrollRange")
+                val color = changeAlpha(
+                    ContextCompat.getColor(requireContext(), R.color.white),
+                    abs(verticalOffset * 1.0f) / totalScrollRange
+                )
+                toolbar.setBackgroundColor(color)
+                val shouldShowToolbar = totalVerticalOffset >= totalScrollRange
+                if (isToolbarShown != shouldShowToolbar) {
+                    isToolbarShown = shouldShowToolbar
+                    appbar.isActivated = shouldShowToolbar
+                    toolbarLayout.isTitleEnabled = shouldShowToolbar
+                    if (isToolbarShown) {
+                        Common.setStatusColorDark(requireActivity())
+                    } else {
+                        Common.setStatusColorLight(requireActivity())
+                    }
+                }
+
+                //toolbarLayout.setStatusBarScrimColor(color)
+            })
             homePhotosList.addOnScrollListener(object : RecyclerView.OnScrollListener() {
                 override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
                     super.onScrolled(recyclerView, dx, dy)
-                    Log.v("dy", "dy : $dy")
-                    val shouldShowToolbar = dy > 0
-                    if (isToolbarShown != shouldShowToolbar) {
-                        isToolbarShown = shouldShowToolbar
-
-                        // Use shadow animator to add elevation if toolbar is shown
-                        appbar.isActivated = shouldShowToolbar
-
-                        // Show the plant name if toolbar is shown
-                        toolbarLayout.isTitleEnabled = shouldShowToolbar
-                        if (isToolbarShown) {
-                            Common.setStatusColorDark(activity!!)
-                        } else {
-                            Common.setStatusColorLight(activity!!)
-                        }
-                    }
+                    //Log.v("dy", "dy : $dy")
+//                    val shouldShowToolbar = dy > 0
+//                    if (isToolbarShown != shouldShowToolbar) {
+//                        isToolbarShown = shouldShowToolbar
+//
+//                        // Use shadow animator to add elevation if toolbar is shown
+//                        appbar.isActivated = shouldShowToolbar
+//
+//                        // Show the plant name if toolbar is shown
+//                        toolbarLayout.isTitleEnabled = shouldShowToolbar
+//                        if (isToolbarShown) {
+//                            Common.setStatusColorDark(activity!!)
+//                        } else {
+//                            Common.setStatusColorLight(activity!!)
+//                        }
+//                    }
 
                 }
             })
@@ -72,6 +103,14 @@ class HomeFragment : Fragment() {
 //        setHasOptionsMenu(true)
         Common.setStatusColorLight(requireActivity())
         return binding.root
+    }
+
+    fun changeAlpha(color: Int, fraction: Float): Int {
+        val red: Int = Color.red(color)
+        val green: Int = Color.green(color)
+        val blue: Int = Color.blue(color)
+        val alpha = (Color.alpha(color) * fraction)
+        return Color.argb(alpha.toInt(), red, green, blue)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -95,6 +134,12 @@ class HomeFragment : Fragment() {
             photosList.add("Photo $i")
         }
         return photosList
+    }
+
+    @Override
+    override fun onSaveInstanceState(savedInstanceState: Bundle) {
+        savedInstanceState.putBoolean(isToolbarShownKey, isToolbarShown)
+        super.onSaveInstanceState(savedInstanceState)
     }
 
 }
--- app/src/main/java/id/co/app/source/ui/main/MainActivity.kt
+++ app/src/main/java/id/co/app/source/ui/main/MainActivity.kt
@@ -4,9 +4,7 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil.setContentView
 import androidx.lifecycle.LiveData
-import androidx.lifecycle.Observer
 import androidx.navigation.NavController
-import androidx.navigation.ui.setupActionBarWithNavController
 import dagger.hilt.android.AndroidEntryPoint
 import id.co.app.source.R
 import id.co.app.source.databinding.MainActivityBinding
