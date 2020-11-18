30efd4c8777af20d39ae05ed97eedfb088751d87
hendrysyamsudin <hendry.syamsudin@gmail.com>
2020-11-18 17:53:08 +0700

    Add new home page

--- app/src/main/java/id/co/app/source/ui/home/HomeFragment.kt
+++ app/src/main/java/id/co/app/source/ui/home/HomeFragment.kt
@@ -45,9 +45,9 @@ class HomeFragment : Fragment() {
             toolbarLayout.scrimVisibleHeightTrigger = scrimHeightTrigger.toInt()
             appbar.addOnOffsetChangedListener(OnOffsetChangedListener { appBarLayout, verticalOffset ->
                 val totalScrollRange = appBarLayout.totalScrollRange
-                val midTotalScrollRange = totalScrollRange / 2
+//                val midTotalScrollRange = totalScrollRange / 2
                 val totalVerticalOffset = verticalOffset * -1
-                Log.v("verticalOffset", "verticalOffset : $totalVerticalOffset , $totalScrollRange")
+//                Log.v("verticalOffset", "verticalOffset : $totalVerticalOffset , $totalScrollRange")
 //                val color = changeAlpha(
 //                    ContextCompat.getColor(requireContext(), R.color.white),
 //                    abs(verticalOffset * 1.0f) / totalScrollRange
