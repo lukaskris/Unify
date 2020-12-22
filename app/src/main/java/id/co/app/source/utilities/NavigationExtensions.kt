e7bd8ebd1904e4682e4dc05161cdb8b23904a295
hendrysyamsudin <hendry.syamsudin@gmail.com>
2020-12-22 17:35:09 +0700

    Add created and modified date

--- app/src/main/java/id/co/app/source/utilities/NavigationExtensions.kt
+++ app/src/main/java/id/co/app/source/utilities/NavigationExtensions.kt
@@ -1,3 +1,9 @@
+/*
+ * Created by Hendry Syamsudin on 22/12/20 17:34
+ * Copyright (c) APP Sinar Mas 2020. All rights reserved.
+ * Last modified 22/12/20 17:34
+ */
+
 package id.co.app.source.utilities
 
 import android.content.Intent
